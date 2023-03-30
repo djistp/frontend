21df8449c77b02bc4532832a5fe6aa760581e80d
djistp <eduardoarodriguez272@gmail.com>
2023-03-30 13:13:22 -0600

    plswork

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Link, Route, Routes } from 'react-router-dom';
+import {Home} from "./Home"
+import { Podcast } from './Podcast';
+import { MovieList } from './MovieList';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <Link to="/Podcast">Podcasts</Link>
+          <li>
+            <Link to="/MovieList">Movies</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/Podcast" element={<Podcast/>} />
+        <Route path="/MovieList" element={<MovieList/>} />
+      </Routes>
+    
+    </>
   );
 }
 
